efb8a067abb8615ab557657731ff898214f3ab72
jayholy <55398504+jayholy@users.noreply.github.com>
2022-09-07 15:03:23 +0000

    stats

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styles from '../style'
 import { stats } from '../constants'
+import NumberCounter from "number-counter";
 
 const Stats = () => {
   return (
@@ -8,9 +9,13 @@ const Stats = () => {
       className={`${styles.flexCenter} flex flex-row mb-6 sm:mb-20 flex-wrap`}
     >
       {stats.map((stat) => (
-        <div key={stat.id} className={`flex flex-1 items-center justify-start flex-row m-3`}>
+        <div
+          key={stat.id}
+          className={`flex flex-1 items-center justify-center flex-row m-3`}
+        >
           <h4 className="text-white font-poppins font-semibold xs:leading-[53px] leading-[43px] text-[30px] xs:text-[40px]">
             {stat.value}
+            
           </h4>
           <p className="text-gradient uppercase ml-3 font-poppins font-normal xs:leading-[26px] leading-[21px] text-[15px] xs:text-[20px]">
             {stat.title}
--- src/constants/index.js
+++ src/constants/index.js
@@ -89,17 +89,17 @@ export const stats = [
   {
     id: "stats-1",
     title: "User Active",
-    value: "3800+",
+    value: "38700",
   },
   {
     id: "stats-2",
     title: "Trusted by Company",
-    value: "230+",
+    value: "2310",
   },
   {
     id: "stats-3",
     title: "Transaction",
-    value: "$230M+",
+    value: "$830M",
   },
 ];
 

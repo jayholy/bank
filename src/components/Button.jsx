c0344782f7267001e7206d2bd5f65d526cd1befe
jayholy <55398504+jayholy@users.noreply.github.com>
2022-09-07 16:53:47 +0000

    deal

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = ({styles}) => {
   return (
-    <button type='button' className={`py-4 md:w-[15rem] w-full px-6 bg-blue-gradient font-poppins rounded-md mt-4 font-medium text-primary text-[18px] outline-none`}>
+    <button type='button' className={`py-4 md:w-[10rem] w-full px-6 bg-blue-gradient font-poppins rounded-md mt-4 font-medium text-primary text-[18px] outline-none`}>
 Get Started
     </button>
   )
--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import styles, {layout} from '../style'
+import { card  } from '../assets'
+import Button from './Button';
 
 const CardDeal = () => {
   return (
-    <div>CardDeal</div>
-  )
+    <section className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>
+          {" "}
+          Find a better card deal <br className="hidden sm:block" /> in few easy
+          steps.
+        </h2>
+        <p className={`max-w-[470px] mt-5 ${styles.paragraph} `}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Exercitationem ea maiores velit, eveniet modi beatae illum repellat
+          laboriosam adipisci voluptates. Nihil illo voluptatum possimus ipsa.
+        </p>
+        <Button styles="mt-10" />
+      </div>
+      <div className={layout.sectionImg}>
+        <img src={card} className="w-[100%] h-[100%]" alt="" />
+      </div>
+    </section>
+  );
 }
 
 export default CardDeal

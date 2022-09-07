885e525b1a97cc91e2ef38024965c85014172cb4
jayholy <55398504+jayholy@users.noreply.github.com>
2022-09-07 09:25:40 +0000

    stats

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -8,11 +8,13 @@ const Stats = () => {
       className={`${styles.flexCenter} flex flex-row mb-6 sm:mb-20 flex-wrap`}
     >
       {stats.map((stat) => (
-        <div key={stat.id} className="">
+        <div key={stat.id} className={`flex flex-1 items-center justify-start flex-row m-3`}>
           <h4 className="text-white font-poppins font-semibold xs:leading-[53px] leading-[43px] text-[30px] xs:text-[40px]">
             {stat.value}
           </h4>
-          <p>{stat.title}</p>
+          <p className="text-gradient uppercase ml-3 font-poppins font-normal xs:leading-[26px] leading-[21px] text-[15px] xs:text-[20px]">
+            {stat.title}
+          </p>
         </div>
       ))}
     </section>

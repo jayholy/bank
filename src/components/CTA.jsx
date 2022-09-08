3f11be57f0e789d38c8a9cb212ed9cab76c825d2
jayholy <55398504+jayholy@users.noreply.github.com>
2022-09-08 14:52:05 +0000

    cta

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import styles from '../style'
+import Button from './Button'
 
 const CTA = () => {
   return (
-    <div>CTA</div>
-  )
+    <section
+      className={`${styles.padding}${styles.marginY} bg-black-gradient-2 sm:flex-row flex-col box-shadow rounded-[20px] ${styles.flexCenter}`}
+    >
+      <div>
+        <h2 className={styles.heading2}>
+          Let's try <span className="text-gradient">our</span> service now
+        </h2>
+        <p className={`max-w-[470px] mt-5 ${styles.paragraph} `}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. In, laborum
+          natus. Libero nisi dolore fuga quam repudiandae recusandae?
+        </p>
+      </div>
+      <div className={`${styles.flexCenter} sm:ml-10 ml-0 sm:mt-0 mb-10`}>
+        <Button  />
+      </div>
+    </section>
+  );
 }
 
 export default CTA
--- src/components/Client.jsx
+++ src/components/Client.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import { clients } from '../constants'
+import styles from '../style'
 
 const Client = () => {
   return (
-    <div className='bg-red-500'>Client</div>
-  )
+    <section className={`${styles.flexCenter} my-4`}>
+      <div className={`flex-wrap w-full ${styles.flexCenter} `}>
+        {clients.map((client) => (
+          <div
+            key={client.id}
+            className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[120px]`}
+          >
+            <img
+              src={client.logo}
+              className="w-[100px] sm:w-[192px] object-contain shadow-2xl border-none cursor-pointer  hover hover:shadow-cyan-500/50 transition-all duration-400 ease-in-out hover:scale-125"
+              alt=""
+            />
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 }
 
 export default Client
